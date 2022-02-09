ab23c9a249a8c90cf4270b15723799c14151289a
Axel <axosar@gmail.com>
2022-02-09 17:15:13 -0600

    nombre

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <Layout>
       <div className="container-fluid home">
-        <h1>TOKAMAC</h1>
+        <h1>TOKAMAK</h1>
         <h4><b>Fusion Energy</b> can produce little waste and requires only small amounts of abundant, naturally-sourced fuel, including elements extracted from seawater. This makes it an attractive option as the world <b>transitions away from the fossil fuels</b> driving climate change.</h4>
       </div>
     </Layout>
