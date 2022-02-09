590e455096111141893b68454660ac588cdad13b
Axel <axosar@gmail.com>
2022-02-09 17:15:45 -0600

    metas

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -4,10 +4,10 @@ function Layout({children}) {
   return (
     <div>
       <Head>
-        <title>TOKAMAC Limitless clean power source</title>
+        <title>TOKAMAK Limitless clean power source</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="TOKAMAC limitless clean power source is possible" />
-        <meta name="keywords" content="tokamac, energy, clean power" />
+        <meta name="description" content="TOKAMAK limitless clean power source is possible" />
+        <meta name="keywords" content="tokamak, energy, clean power" />
         <meta name="author" content="Axel Laurent Obscura Sarzotti" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
