ba662477fe2d410de77e951673c8095e1e45776f
Axel <axosar@gmail.com>
2022-02-09 16:58:25 -0600

    intro

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -6,10 +6,16 @@ function Layout({children}) {
       <Head>
         <title>TOKAMAC Limitless clean power source</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Free Web tutorials for HTML and CSS" />
-        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="description" content="TOKAMAC limitless clean power source is possible" />
+        <meta name="keywords" content="tokamac, energy, clean power" />
+        <meta name="author" content="Axel Laurent Obscura Sarzotti" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;800&family=Lora:wght@400;700&family=Nunito+Sans:wght@200;900&family=Poppins:wght@100;900&family=Titillium+Web:wght@200;300;900&display=swap" rel="stylesheet" /> 
       </Head>
-      {children}
+      <div className="container-fluid m-0 p-0">
+        {children}
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,12 @@ import Layout from "./components/Layout"
 
 function HomePage() {
   return (
-    <Layout>TOKAMAC WEBSITite</Layout>
+    <Layout>
+      <div className="container-fluid home">
+        <h1>TOKAMAC</h1>
+        <h4><b>Fusion Energy</b> can produce little waste and requires only small amounts of abundant, naturally-sourced fuel, including elements extracted from seawater. This makes it an attractive option as the world <b>transitions away from the fossil fuels</b> driving climate change.</h4>
+      </div>
+    </Layout>
   )
 }
 
