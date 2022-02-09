97e46badcc48ffde61e01b81983336288312952b
Axel <axosar@gmail.com>
2022-02-09 16:42:37 -0600

    title

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,6 +1,14 @@
+import Head from "next/head";
+
 function Layout({children}) {
   return (
     <div>
+      <Head>
+        <title>TOKAMAC Limitless clean power source</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Free Web tutorials for HTML and CSS" />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+      </Head>
       {children}
     </div>
   )
