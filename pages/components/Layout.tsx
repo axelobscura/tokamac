06aca3de3138f9d1a8e33f4db0e4dd3c0e26ac69
Axel <axosar@gmail.com>
2022-02-12 20:38:09 -0600

    menu collapse

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -21,6 +21,9 @@ function Layout({children}) {
       <div className="container-fluid m-0 p-0">
         {children}
       </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></script>
     </div>
   )
 }
