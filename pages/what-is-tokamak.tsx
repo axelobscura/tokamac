69dd1c41f41df0b618bbd1ab7240caef8d4fc4c2
Axel <axosar@gmail.com>
2022-02-14 14:25:36 -0600

    styling

--- pages/what-is-tokamak.tsx
+++ pages/what-is-tokamak.tsx
@@ -3,7 +3,7 @@ import Layout from "./components/Layout"
 export default function whatIsTokamac() {
   return(
     <Layout>
-      <div>
+      <div className="what-tokamac">
         <h1>What is a TOKAMAK</h1>
       </div>
     </Layout>
